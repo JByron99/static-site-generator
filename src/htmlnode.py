6f137fc645e96d1c33e14a39182b487058050d86
JByron99 <sbyron@protonmail.com>
2024-10-15 02:42:49 -0400

    leaf node added

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,9 +13,31 @@ class HTMLNode():
         if self.props == None:
             return props_string
         for key in self.props:
-            props_string += f"{key}={self.props[key]} "
+            props_string += f"{key}=\"{self.props[key]}\" "
 
         return props_string[:-1]
 
     def __repr__(self):
         return f"Tag: {self.tag} \nValue: {self.value} \nChildren: {self.children} \nProperties: {self.props}"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        if value == None:
+            raise ValueError("leaf nodes must have a value attribute")
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("leaf nodes must have a value attribute")
+        
+        if self.tag == None:
+            return self.value
+        
+        start = f"<{self.tag}"
+        end = f"</{self.tag}>"
+        if self.props != None:
+            start += " " + self.props_to_html()
+        start += ">"
+
+        return start + self.value + end
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import *
 
 class TestHTMLNode(unittest.TestCase):
     def test_base_case(self):
@@ -12,7 +12,7 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_one_props_to_html(self):
         node = HTMLNode(props={"color":"red"})
-        self.assertEqual(node.props_to_html(), "color=red")
+        self.assertEqual(node.props_to_html(), "color=\"red\"")
 
     def test_many_props_to_html(self):
         three_props = {
@@ -22,7 +22,32 @@ class TestHTMLNode(unittest.TestCase):
                 }
         node = HTMLNode(props=three_props)
 
-        target_string = "color=red font-size=15px font-type=ariel"
+        target_string = "color=\"red\" font-size=\"15px\" font-type=\"ariel\""
 
         self.assertEqual(node.props_to_html(), target_string)
         self.assertNotEqual(node.props_to_html(), target_string + " ")
+
+class TestLeafNode(unittest.TestCase):
+    def test_invalid_leaf(self):
+        try:
+            node = LeafNode()
+        except ValueError:
+            self.assertTrue(True)
+            return
+        self.assertTrue(False)
+
+    def test_plain_text_leaf(self):
+        plain_leaf = LeafNode(value="Plain.")
+        target_html = "Plain."
+        self.assertEqual(plain_leaf.to_html(), target_html)
+
+    def test_p_leaf(self):
+        p_leaf = LeafNode("p", "This is a paragraph of text.")
+        target_html = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(p_leaf.to_html(), target_html)
+
+    def test_a_leaf(self):
+        a_leaf = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        target_html = "<a href=\"https://www.google.com\">Click me!</a>"
+        self.assertEqual(a_leaf.to_html(), target_html)
+
